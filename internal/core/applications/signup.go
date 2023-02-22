21653f9d9ce3a7278cabf5c1183420a1eff503ce
Leonardo Ospina <42081538+Lenstack@users.noreply.github.com>
2023-02-22 15:58:51 -0500

    Update signup.go

--- internal/core/applications/signup.go
+++ internal/core/applications/signup.go
@@ -10,6 +10,8 @@ import (
 
 // SignUp TODO: 1. Get user from request, 2. Validate request, 3. Call SignUp method from UserService, 4. Return success message
 func (m *Microservice) SignUp(wr http.ResponseWriter, req *http.Request) {
+	wr.Header().Set("Content-Type", "application/json")
+
 	body := &models.SignUpRequest{}
 	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
 		wr.WriteHeader(http.StatusBadRequest)
@@ -22,7 +24,6 @@ func (m *Microservice) SignUp(wr http.ResponseWriter, req *http.Request) {
 
 	validateErrors := utils.Validate(body)
 	if len(validateErrors) > 0 {
-		wr.Header().Set("Content-Type", "application/json")
 		wr.WriteHeader(http.StatusBadRequest)
 		err := json.NewEncoder(wr).Encode(validateErrors)
 		if err != nil {
@@ -33,10 +34,13 @@ func (m *Microservice) SignUp(wr http.ResponseWriter, req *http.Request) {
 
 	message, err := m.UserService.SignUp(entities.User{Name: body.Name, Email: body.Email, Password: body.Password})
 	if err != nil {
+		wr.WriteHeader(http.StatusInternalServerError)
+		_, err := wr.Write([]byte(err.Error()))
+		if err != nil {
+			return
+		}
 		return
 	}
-
-	wr.Header().Set("Content-Type", "application/json")
 	wr.WriteHeader(http.StatusOK)
 	err = json.NewEncoder(wr).Encode(message)
 	if err != nil {
