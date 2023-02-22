46c713cc45a35b9cfc054811f5422005568a66dd
Leonardo Ospina <42081538+Lenstack@users.noreply.github.com>
2023-02-22 12:50:49 -0500

    Add Validation Models

---
+++ internal/core/models/signup.go
@@ -0,0 +1,11 @@
+package models
+
+type SignUpRequest struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type SignUpResponse struct {
+	Message string `json:"message"`
+}
