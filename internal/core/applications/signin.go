62c40b8d55cffe3a31f79be60d2cf8ddff97ec04
Leonardo Ospina <42081538+Lenstack@users.noreply.github.com>
2023-02-22 12:50:15 -0500

    Implement Validation Request For Signin - Signup And Add Signout

--- internal/core/applications/signin.go
+++ internal/core/applications/signin.go
@@ -1,10 +1,44 @@
 package applications
 
 import (
+	"encoding/json"
+	"github.com/Lenstack/lensaas-app/internal/core/models"
+	"github.com/Lenstack/lensaas-app/internal/utils"
 	"net/http"
 )
 
 // SignIn TODO: 1. Get email and password from request, 2. Validate request, 3. Call SignIn method from UserService, 4. Return token
-func (m *Microservice) SignIn(responseWriter http.ResponseWriter, request *http.Request) {
-	return
+func (m *Microservice) SignIn(wr http.ResponseWriter, req *http.Request) {
+	body := &models.SignInRequest{}
+	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
+		wr.WriteHeader(http.StatusBadRequest)
+		_, err := wr.Write([]byte(err.Error()))
+		if err != nil {
+			return
+		}
+		return
+	}
+
+	validateErrors := utils.Validate(body)
+	if len(validateErrors) > 0 {
+		wr.Header().Set("Content-Type", "application/json")
+		wr.WriteHeader(http.StatusBadRequest)
+		err := json.NewEncoder(wr).Encode(validateErrors)
+		if err != nil {
+			return
+		}
+		return
+	}
+
+	message, err := m.UserService.SignIn(body.Email, body.Password)
+	if err != nil {
+		return
+	}
+
+	wr.Header().Set("Content-Type", "application/json")
+	wr.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(wr).Encode(message)
+	if err != nil {
+		return
+	}
 }
--- internal/core/applications/signout.go
+++ internal/core/applications/signout.go
@@ -3,6 +3,6 @@ package applications
 import "net/http"
 
 // SignOut TODO: 1. Get user from request, 2. Validate request, 3. Call SignOut method from UserService, 4. Return success message
-func (m *Microservice) SignOut(responseWriter http.ResponseWriter, request *http.Request) {
+func (m *Microservice) SignOut(wr http.ResponseWriter, req *http.Request) {
 	return
 }
