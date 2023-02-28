8e05b2658685558ac7eb94322f6b522b2fbcd264
Leonardo Ospina <42081538+Lenstack@users.noreply.github.com>
2023-02-27 22:29:59 -0500

    Update middleware.go

--- internal/core/applications/middleware.go
+++ internal/core/applications/middleware.go
@@ -76,10 +76,10 @@ func (m *Microservice) MiddlewareLogger(next http.Handler) http.Handler {
 func (m *Microservice) MiddlewareCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		// Stop here for a Preflighted OPTIONS request.
 		if r.Method == "OPTIONS" {
 			return
